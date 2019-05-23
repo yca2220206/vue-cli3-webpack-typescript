14dc45994d3db7ec030aafb07cdbd92d8e3fda84
Mr.liu <leo.liu@asiahr.com>
2019-05-23 18:08:46 +0800

    change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld :msg="msg"/>
   </div>
 </template>
 
@@ -10,6 +9,11 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
+  data() {
+     return {
+       msg: '1234',
+     }
+  },
   components: {
     HelloWorld,
   },
